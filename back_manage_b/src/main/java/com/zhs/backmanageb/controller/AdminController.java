faf205f664acbe27eff6a128c6c831e67562dd34
zhanghslq <zhanghslq@163.com>
2020-07-30 22:12:15 +0800

    用户模块待完善

--- back_manage_b/src/main/java/com/zhs/backmanageb/controller/AdminController.java
+++ back_manage_b/src/main/java/com/zhs/backmanageb/controller/AdminController.java
@@ -1,8 +1,19 @@
 package com.zhs.backmanageb.controller;
 
+import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
+import com.zhs.backmanageb.common.Result;
+import com.zhs.backmanageb.entity.Page;
+import com.zhs.backmanageb.entity.Role;
+import com.zhs.backmanageb.service.AdminService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author: zhs
@@ -10,16 +21,41 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RequestMapping("/admin")
 @RestController
+@Api(tags = "管理员管理")
 public class AdminController {
 
+    @Resource
+    private AdminService adminService;
+
     @PostMapping("/login")
-    public void login(){
+    @ApiOperation("登陆")
+    public Result<String> login(String username,String password){
         System.out.println("login");
+
+        return Result.success("");
+    }
+    @PostMapping("logout")
+    @ApiOperation("退出")
+    public Result<Boolean> logout(Long adminId){
+
+        return Result.success(true);
+    }
+    @ApiOperation("根据管理员id查询拥有角色")
+    @PostMapping("list_role/by_admin_id")
+    public Result<List<Role>> listRoleByAdminId(@RequestParam Long adminId){
+        return Result.success(adminService.listRoleByAdminId(adminId));
     }
 
-    @PostMapping("/search")
-    public String search(){
-
-        return "";
+    @PostMapping("list_page/by_admin_id")
+    @ApiOperation("根据管理员id查询拥有页面权限")
+    public Result<List<Page>> listPageByAdminId(@RequestParam Long adminId){
+        return Result.success(adminService.listPageByAdminId(adminId));
     }
+
+    @PostMapping("list_page/by_role_id")
+    @ApiOperation("根据角色id查询页面权限")
+    public Result<List<Page>> listPageByRoleId(@RequestParam Long roleId){
+        return Result.success(adminService.listPageByRoleId(roleId));
+    }
+
 }
--- back_manage_b/src/main/java/com/zhs/backmanageb/service/impl/AdminServiceImpl.java
+++ back_manage_b/src/main/java/com/zhs/backmanageb/service/impl/AdminServiceImpl.java
@@ -1,13 +1,17 @@
 package com.zhs.backmanageb.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.zhs.backmanageb.entity.Admin;
-import com.zhs.backmanageb.entity.CommonData;
+import com.zhs.backmanageb.entity.*;
 import com.zhs.backmanageb.mapper.AdminMapper;
 import com.zhs.backmanageb.mapper.CommonDataMapper;
-import com.zhs.backmanageb.service.AdminService;
-import com.zhs.backmanageb.service.CommonDataService;
+import com.zhs.backmanageb.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -20,4 +24,64 @@ import org.springframework.stereotype.Service;
 @Service
 public class AdminServiceImpl extends ServiceImpl<AdminMapper, Admin> implements AdminService {
 
+    @Autowired
+    private AdminRoleService adminRoleService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private PageService pageService;
+
+    @Autowired
+    private RolePageService rolePageService;
+
+
+
+    @Override
+    public List<Role> listRoleByAdminId(Long adminId) {
+        ArrayList<Role> result = new ArrayList<>();
+        QueryWrapper<AdminRole> adminRoleQueryWrapper = new QueryWrapper<>();
+        adminRoleQueryWrapper.eq("admin_id",adminId);
+        List<AdminRole> list = adminRoleService.list(adminRoleQueryWrapper);
+        if(list.size()==0){
+            return result;
+        }
+        List<Long> roleIds = list.stream().map(AdminRole::getRoleId).collect(Collectors.toList());
+        return roleService.listByIds(roleIds);
+    }
+
+    @Override
+    public List<Page> listPageByAdminId(Long adminId) {
+        ArrayList<Page> result = new ArrayList<>();
+        QueryWrapper<AdminRole> adminRoleQueryWrapper = new QueryWrapper<>();
+        adminRoleQueryWrapper.eq("admin_id",adminId);
+        List<AdminRole> list = adminRoleService.list(adminRoleQueryWrapper);
+        if(list.size()==0){
+            return result;
+        }
+        List<Long> roleIds = list.stream().map(AdminRole::getRoleId).collect(Collectors.toList());
+        QueryWrapper<RolePage> rolePageQueryWrapper = new QueryWrapper<>();
+        rolePageQueryWrapper.in("role_id",roleIds);
+        List<RolePage> rolePages = rolePageService.list(rolePageQueryWrapper);
+        if(rolePages.size()==0){
+            return result;
+        }
+        List<Long> pageIds = rolePages.stream().map(RolePage::getPageId).collect(Collectors.toList());
+
+        return pageService.listByIds(pageIds);
+    }
+
+    @Override
+    public List<Page> listPageByRoleId(Long roleId) {
+        ArrayList<Page> result = new ArrayList<>();
+        QueryWrapper<RolePage> rolePageQueryWrapper = new QueryWrapper<>();
+        rolePageQueryWrapper.eq("role_id",roleId);
+        List<RolePage> rolePages = rolePageService.list(rolePageQueryWrapper);
+        if(rolePages.size()==0){
+            return result;
+        }
+        List<Long> pageIds = rolePages.stream().map(RolePage::getPageId).collect(Collectors.toList());
+        return pageService.listByIds(pageIds);
+    }
 }

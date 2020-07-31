bb83c03f849145a0566268477a7a9df60c368259
zhs <haisong@tiplus.cn>
2020-07-31 11:16:28 +0800

    工作记录管理

--- back_manage_b/src/main/java/com/zhs/backmanageb/controller/ExperienceRecordController.java
+++ back_manage_b/src/main/java/com/zhs/backmanageb/controller/ExperienceRecordController.java
@@ -1,9 +1,17 @@
 package com.zhs.backmanageb.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.zhs.backmanageb.common.Result;
+import com.zhs.backmanageb.entity.ExperienceRecord;
+import com.zhs.backmanageb.service.ExperienceRecordService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +21,46 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zhs
  * @since 2020-07-21
  */
+@Api(tags = "工作记录管理")
 @RestController
 @RequestMapping("/experienceRecord")
 public class ExperienceRecordController {
+
+    @Resource
+    private ExperienceRecordService experienceRecordService;
+
+    @ApiOperation("插入工作记录")
+    @PostMapping("insert")
+    public Result<Boolean> insert(@RequestBody ExperienceRecord experienceRecord){
+        experienceRecordService.save(experienceRecord);
+        return Result.success(true);
+    }
+
+    @PostMapping("insertBatch")
+    @ApiOperation("批量插入")
+    public Result<Boolean> insert(@RequestBody List<ExperienceRecord> experienceRecords){
+        return Result.success(experienceRecordService.saveBatch(experienceRecords));
+    }
+    @PostMapping("delete")
+    @ApiOperation("删除")
+    public Result<Boolean> delete(@RequestParam Long id){
+        experienceRecordService.removeById(id);
+        return Result.success(true);
+    }
+    @PostMapping("update")
+    @ApiOperation("根据id修改")
+    public Result<Boolean> update(@RequestBody ExperienceRecord experienceRecord){
+        experienceRecordService.updateById(experienceRecord);
+        return Result.success(true);
+    }
+
+    @PostMapping("updateBatch")
+    @ApiOperation("批量根据id修改")
+    public Result<Boolean> updateBatch(@RequestBody List<ExperienceRecord> experienceRecords){
+        experienceRecordService.updateBatchById(experienceRecords);
+        return Result.success(true);
+    }
+
 
 }
 

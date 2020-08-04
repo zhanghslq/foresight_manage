6d46ade00d58aa6dab565aae9553af3b411a33e9
zhs <haisong@tiplus.cn>
2020-08-04 11:37:34 +0800

    用户修改

--- back_manage_b/src/main/java/com/zhs/backmanageb/entity/Role.java
+++ back_manage_b/src/main/java/com/zhs/backmanageb/entity/Role.java
@@ -7,6 +7,8 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableField;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -36,6 +38,7 @@ public class Role implements Serializable {
     @ApiModelProperty(value = "角色描述")
     private String description;
 
+    @JsonIgnore
     @ApiModelProperty(value = "删除标志")
     @TableLogic
     private Integer deleted;
